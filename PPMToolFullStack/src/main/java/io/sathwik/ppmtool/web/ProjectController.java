5a298c10bc89eb71562e9b8c32db581308f69e8a
sathwik227 <sathwik.reddy227@gmail.com>
2021-06-12 21:50:26 +0530

    Validations and Port Change

--- PPMToolFullStack/src/main/java/io/sathwik/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/sathwik/ppmtool/web/ProjectController.java
@@ -5,6 +5,7 @@ import io.sathwik.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +21,12 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> creatNewProject(@Valid @RequestBody Project project){
+    public ResponseEntity<?> creatNewProject(@Valid @RequestBody Project project, BindingResult result){
+        if(result.hasErrors()){
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<>(project, HttpStatus.CREATED);
+        return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
 
 }
